bd21622ca76dcfb10801c89fc4ec322190242312
barrettotte <barrettotte@gmail.com>
2022-08-13 18:50:23 -0400

    rewrite random number generator to true LCG

--- src/rand.asm
+++ src/rand.asm
@@ -16,17 +16,30 @@ srand:
         ret                                 ; end srand subroutine
 
 ; *****************************************************************************
-; rand32 - Generate unsigned pseudo-random 32-bit integer
+; rand32 - Generate unsigned pseudo-random 32-bit integer using 
+;   Linear Congruential Generator (LCG) with values from ANSI C.
+;   
+;   $X_{n+1} = (aX_n + c) % m$
+;   
+;   where: 
+;     - $X_{n+1}$ is the next seed (bits 30..16 of generated value)
+;     - $a$ is the multiplier, an arbitrary large prime. In ANSI C, this is 1103515245
+;     - $X_n$ is the current seed value
+;     - $c$ is the increment, an arbitary prime. In ANSI C, this is 12345
+;     - $m$ is the modulus. In ANSI C, this is 2^31 = 1 << 31 = 2147483648
 ;
-; eax (ret) - generated number
+; eax (ret) - pseudo random number
 ; *****************************************************************************
 rand32:
         push rdx                            ; save rdx
-        mov eax, 0x377AA                    ; divide by arbitrary number
-        imul dword [seed]                   ; generate base random
-        add eax, 0x1AAC4A                   ; add arbitrary number
-        mov dword [seed], eax               ; save seed for next call
-        ror eax, 8                          ; grab lowest digit
+
+        mov eax, 0x41C64E6D                 ; a
+        imul dword [seed]                   ; (a * seed)
+        add eax, 0x3039                     ; (a * seed) + c
+        shr eax, 16                         ; isolate output bits
+        and eax, 0x7FFF                     ; (a * seed) % m
+        call srand                          ; store new seed for next generation
+
         pop rdx                             ; restore rdx
         ret                                 ; end rand32 subroutine
 
@@ -34,10 +47,11 @@ rand32:
 ; rand32_range - Generate random number in range 0 through (n-1)
 ;
 ; eax (arg) - max number
-; eax (ret) - generated number
+; eax (ret) - pseudo random number
 ; *****************************************************************************
 rand32_range:
         push rbx                            ; save rbx
+        push rcx                            ; save rcx
         push rdx                            ; save rdx
 
         mov ebx, eax                        ; max value
@@ -47,5 +61,6 @@ rand32_range:
         mov eax, edx                        ; remainder
 
         pop rdx                             ; restore rdx
+        pop rcx                             ; restore rcx
         pop rbx                             ; restore rbx
         ret                                 ; end rand32_range subroutine
